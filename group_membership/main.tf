d3c40d9c78bbacc17f72b37fd269cba29793021c
annakamienska-mercer <anna.mercer@gsa.gov>
2025-06-26 09:17:20 -0500

    removed group variable

--- group_membership/main.tf
+++ group_membership/main.tf
@@ -1,15 +1,8 @@
 
-locals {
-  gl_groups = {
-    for group in var.groups : group.full_path => {
-      id = group.id
-  } }
-
-}
 
 resource "gitlab_group_membership" "group_membership" {
   for_each     = var.memberships
   user_id      = each.value.user_id
-  group_id     = local.gl_groups["${each.value.parent_group_path}/${each.value.group_name}/roles/${each.value.access_level}"].id
+  group_id     = each.value.group_id
   access_level = trimsuffix(each.value.access_level, "s")
 }
