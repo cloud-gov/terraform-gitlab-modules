99776562c90d9d554842b3cd2c5607cb0e22acb8
annakamienska-mercer <anna.mercer@gsa.gov>
2025-06-26 08:46:21 -0500

    remove data sources

--- group_membership/main.tf
+++ group_membership/main.tf
@@ -2,7 +2,7 @@
 locals {
   gl_groups = {
     for group in var.groups : group.full_path => {
-      id   = group.id
+      id = group.id
   } }
 
   gl_users = {
@@ -14,7 +14,7 @@ locals {
 
 resource "gitlab_group_membership" "group_membership" {
   for_each     = var.memberships
-  user_id      = local.gl_users["${each.value.user_name}"].id
+  user_id      = each.value.user_id
   group_id     = local.gl_groups["${each.value.parent_group_path}/${each.value.group_name}/roles/${each.value.access_level}"].id
   access_level = trimsuffix(each.value.access_level, "s")
 }
--- group_membership/variables.tf
+++ group_membership/variables.tf
@@ -3,7 +3,7 @@ variable "memberships" {
   description = "Map of memberships"
   type = map(object({
     group_name        = string
-    user_name         = string
+    user_id           = string
     access_level      = string
     parent_group_path = string
     })
@@ -13,8 +13,8 @@ variable "memberships" {
 variable "groups" {
   description = "Map of groups"
   type = map(object({
-    full_path  = string
-    id         = string
+    full_path = string
+    id        = string
     })
   )
 }
