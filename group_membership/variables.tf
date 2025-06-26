8e20d0c0efaddef2c293e1220785c75012f1f8f4
annakamienska-mercer <anna.mercer@gsa.gov>
2025-06-26 09:59:44 -0500

    changed group name to group id

--- group_membership/variables.tf
+++ group_membership/variables.tf
@@ -2,7 +2,7 @@
 variable "memberships" {
   description = "Map of memberships"
   type = map(object({
-    group_name        = string
+    group_id          = string
     user_id           = string
     access_level      = string
     parent_group_path = string
