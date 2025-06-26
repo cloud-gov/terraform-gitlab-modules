b537783ea69e899332c508b4ae6e82164924767e
annakamienska-mercer <anna.mercer@gsa.gov>
2025-06-26 10:16:12 -0500

    removed unused variable

--- group_membership/variables.tf
+++ group_membership/variables.tf
@@ -2,10 +2,9 @@
 variable "memberships" {
   description = "Map of memberships"
   type = map(object({
-    group_id          = string
-    user_id           = string
-    access_level      = string
-    parent_group_path = string
+    group_id     = string
+    user_id      = string
+    access_level = string
     })
   )
 }
