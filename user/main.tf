b0d9f1fb7439bff90f3679395d25f162812a580e
Ryan Ahearn <ryan.ahearn@gsa.gov>
2025-07-29 16:47:56 -0400

    Switch normal use to using 'force_random_password'

--- user/main.tf
+++ user/main.tf
@@ -2,17 +2,21 @@
 resource "gitlab_user" "user" {
   for_each = var.users
 
-  name           = each.value.name
-  username       = each.value.username
-  email          = each.value.email
-  reset_password = each.value.reset_password
+  name                  = each.value.name
+  username              = each.value.username
+  email                 = each.value.email
+  reset_password        = each.value.reset_password
+  force_random_password = each.value.force_random_password
   # optional
   can_create_group  = each.value.can_create_group
   is_admin          = each.value.is_admin
   is_external       = each.value.is_external
   note              = each.value.note
-  password          = each.value.password
   projects_limit    = each.value.projects_limit
   skip_confirmation = each.value.skip_confirmation
   state             = each.value.state
+
+  lifecycle {
+    ignore_changes = [reset_password, force_random_password]
+  }
 }
--- user/variables.tf
+++ user/variables.tf
@@ -8,14 +8,14 @@ variable "users" {
     email    = string
 
     # optional
-    can_create_group  = optional(bool, false)
-    is_admin          = optional(bool, false)
-    is_external       = optional(bool, false)
-    note              = optional(string)
-    password          = optional(string)
-    projects_limit    = optional(number)
-    reset_password    = optional(bool)
-    skip_confirmation = optional(bool)
-    state             = optional(string)
+    can_create_group      = optional(bool, false)
+    is_admin              = optional(bool, false)
+    is_external           = optional(bool, false)
+    reset_password        = optional(bool, false)
+    force_random_password = optional(bool, true)
+    note                  = optional(string)
+    projects_limit        = optional(number)
+    skip_confirmation     = optional(bool)
+    state                 = optional(string)
   }))
 }
